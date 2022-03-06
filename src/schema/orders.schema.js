4d00946e85c58b48b03cf48102c69176dfa6601e
Marcos <marcosmrs8@hotmail.com>
2022-03-06 18:39:24 -0300

    remove comments

--- src/schema/orders.schema.js
+++ src/schema/orders.schema.js
@@ -1,23 +1,10 @@
 import mongoose from 'mongoose';
 
-// const userSchema = new mongoose.Schema (
-//     {
-//         id: String,
-//         value: Number,
-//         name: String
-//     }
-// )
 const orderSchema = new mongoose.Schema(
     {
         date: String,
         total_value: Number,
-        oportunity:[
-            // id: String,
-            // value: Number,
-            // name: String
-            // // type: Array,
-            // // of: userSchema
-        ]
+        oportunity:[]
     }
 )
 
